44b60b38ecc2086fd87487f77e355db188484aec
banuka <banukaramanayaka76@gmail.com>
2024-02-06 13:10:18 +0530

    other:trpc setup

--- app/page.js
+++ app/page.js
@@ -1,6 +1,13 @@
+'use client'
+
+import { trpc } from "@/trpc/trpc";
 
 export default function Home() {
+  const data = trpc.userList.useQuery()
   return (
-    <h1>hiya</h1>
+    <>
+      <h1>hiya</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </>
   );
 }
---
+++ server/index.js
@@ -0,0 +1,31 @@
+import { z } from 'zod';
+import { publicProcedure, router } from './trpc';
+
+export const appRouter = router({
+    userList: publicProcedure
+        .query(async () => {
+            // Retrieve users from a datasource, this is an imaginary database
+            // const users = await db.user.findMany();
+
+            // const users: User[]
+            return [10, 20, 30];
+        }),
+    userCreate: publicProcedure
+        .input(z.object({ name: z.string() }))
+        .mutation(async (opts) => {
+            // const { input } = opts;
+
+            // const input: {
+            //     name: string;
+            // }
+            // // Create a new user in the database
+            // const user = await db.user.create(input);
+            // const user: {
+            //     name: string;
+            //     id: string;
+            // }
+            return user;
+        }),
+});
+
+// export type AppRouter = typeof appRouter;
